6ed388e48941b8d5f55f4e7b252651126a402314
Tac Tacelosky <tacman@gmail.com>
2023-11-12 10:27:45 -0500

    initial version

---
+++ src/Controller/AppController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Kernel;
+use App\Repository\ProductRepository;
+use Survos\BarcodeBundle\Service\BarcodeService;
+use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+
+    public function __construct(private BarcodeService $barcodeService)
+    {
+    }
+
+    #[Route('/', name: 'app_app')]
+    public function index(Request $request, ProductRepository $productRepository): Response
+    {
+        $string = $request->get('q', 'abcdefg');
+
+        $extensionCheck  = array_reduce(['gd', 'imagick'], function(array $carry, $ext) {
+            $carry[$ext] = extension_loaded($ext);
+            return $carry;
+        }, []);
+
+        return $this->render('app/index.html.twig', [
+            'symfonyVersion' => Kernel::VERSION,
+            'string' => $string,
+            'products' => $productRepository->findAll(),
+            'extensions' => $extensionCheck,
+            'generators' => $this->barcodeService->getGenerators()
+        ]);
+    }
+
+    #[Route('/{generatorCode}/demo', name: 'app_demo')]
+    #[Template('/app/demo.html.twig')]
+    public function demo(string $generatorCode, BarcodeService $barcodeService)
+    {
+        $generatorClass = $barcodeService->getGeneratorClass($generatorCode);
+        return [
+            'imageFormat' => $barcodeService->getImageFormat($generatorClass),
+            'generatorCode' => $generatorCode,
+            'generatorClass' => $generatorClass,
+            'types' => $barcodeService->getGeneratorTypes()
+        ];
+    }
+
+}
---
+++ src/Entity/Country.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CountryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CountryRepository::class)]
+class Country
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 55)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 2)]
+    private ?string $alpha2 = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $alpha3 = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAlpha2(): ?string
+    {
+        return $this->alpha2;
+    }
+
+    public function setAlpha2(string $alpha2): static
+    {
+        $this->alpha2 = $alpha2;
+
+        return $this;
+    }
+
+    public function getAlpha3(): ?string
+    {
+        return $this->alpha3;
+    }
+
+    public function setAlpha3(string $alpha3): static
+    {
+        $this->alpha3 = $alpha3;
+
+        return $this;
+    }
+}
