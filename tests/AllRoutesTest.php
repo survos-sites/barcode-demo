d466c6d3ea8fb965e3ec51cb4cfe00868fea63d4
Tac Tacelosky <tacman@gmail.com>
2025-02-09 06:53:56 -0500

    ignore monitor routes

--- tests/AllRoutesTest.php
+++ tests/AllRoutesTest.php
@@ -3,8 +3,17 @@
 namespace App\Tests;
 
 use Pierstoval\SmokeTesting\SmokeTestStaticRoutes;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 
 class AllRoutesTest extends SmokeTestStaticRoutes
 {
-    // That's all!
+
+    // Your smoke testing class that extends SmokeTestStaticRoutes
+
+    protected function beforeRequest(KernelBrowser $client, string $routeName, string $routePath): void
+    {
+        if (in_array($routeName, ['monitor.internal_health', 'monitor.health'])) {
+            self::markTestSkipped('Untestable route');
+        }
+    }
 }
